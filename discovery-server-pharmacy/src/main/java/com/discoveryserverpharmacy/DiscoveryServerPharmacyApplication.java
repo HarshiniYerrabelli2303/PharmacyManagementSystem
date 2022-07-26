35df33937187963998af1d86eb164b4654d8eefd
Meghana G R <meghana.g-r@capgemini.com>
2022-07-26 09:36:24 +0530

    discovery server commit

---
+++ discovery-server-pharmacy/src/main/java/com/discoveryserverpharmacy/DiscoveryServerPharmacyApplication.java
@@ -0,0 +1,15 @@
+package com.discoveryserverpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerPharmacyApplication.class, args);
+	}
+
+}
---
+++ discovery-server-pharmacy/src/test/java/com/discoveryserverpharmacy/DiscoveryServerPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discoveryserverpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
