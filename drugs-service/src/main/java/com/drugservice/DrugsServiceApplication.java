dd31b7ecdb3fe6845be697d93a08f87b9277e2a5
Meghana G R <meghana.g-r@capgemini.com>
2022-07-26 09:38:22 +0530

    drug service make it as client

--- drugs-service/src/main/java/com/drugservice/DrugsServiceApplication.java
+++ drugs-service/src/main/java/com/drugservice/DrugsServiceApplication.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package com.drugservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DrugsServiceApplication {
 
 	public static void main(String[] args) {
