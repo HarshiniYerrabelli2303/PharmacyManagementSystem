f062b1ad9a67b9bd4216a8aeb6b5a9c169b941f3
Meghana G R <meghana.g-r@capgemini.com>
2022-07-27 12:04:37 +0530

    api gateway commit

---
+++ pharmacy-api-gateway/src/main/java/com/pharmacymanagementsystem/pharmacyapigateway/PharmacyApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.pharmacymanagementsystem.pharmacyapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PharmacyApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyApiGatewayApplication.class, args);
+	}
+
+}
---
+++ pharmacy-api-gateway/src/test/java/com/pharmacymanagementsystem/pharmacyapigateway/PharmacyApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacymanagementsystem.pharmacyapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
