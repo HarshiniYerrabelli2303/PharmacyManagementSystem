0720dbf063b923c0bd96f665f6511b063d61177e
Meghana G R <meghana.g-r@capgemini.com>
2022-08-02 14:20:00 +0530

    doctor service commit

--- doctor-service/src/main/java/com/doctorservice/DoctorServiceApplication.java
+++ doctor-service/src/main/java/com/doctorservice/DoctorServiceApplication.java
@@ -2,11 +2,13 @@ package com.doctorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableEurekaClient
+
 @SpringBootApplication
+@EnableEurekaClient
+@EnableWebMvc
 public class DoctorServiceApplication {
 
 	public static void main(String[] args) {
