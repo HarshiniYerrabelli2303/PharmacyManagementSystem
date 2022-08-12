24cf21333bdd53d0c9bb3904fe4d1da7380029bc
Saivarma9 <103590637+Saivarma9@users.noreply.github.com>
2022-08-12 10:58:09 +0530

    Update AdminServiceApplicationTests.java

--- Admin-Service/src/test/java/com/adminservice/AdminServiceApplicationTests.java
+++ Admin-Service/src/test/java/com/adminservice/AdminServiceApplicationTests.java
@@ -1,13 +1,44 @@
 package com.adminservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.adminservice.entity.Admin;
+import com.adminservice.repository.AdminRepository;
+import com.adminservice.service.AdminService;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class AdminServiceApplicationTests {
+public class AdminServiceApplicationTests {
+  
+	@Autowired
+	private AdminService service;
+	
+	@MockBean
+	private AdminRepository repository;
+	
 
 	@Test
-	void contextLoads() {
-	}
+	public void testRegisterCustomer() {
+	Admin admin = new Admin();
 
+	admin.setFirstName("raju");
+	admin.setLastName("raju");
+	admin.setEmail("raju12@gmail.com");
+	admin.setPhone("9909890712");
+	admin.setGender("Male");
+	admin.setPassword("raju123");
+
+	Mockito.when(AdminRepository.save(admin)).thenReturn(admin);
+	}
+		
+	
+
+	
 }
